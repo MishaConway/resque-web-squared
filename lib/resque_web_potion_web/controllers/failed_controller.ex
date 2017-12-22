ea5596395b8c3e440cfdd9d9e1edc38704e6c0e7
Misha Conway <mishaAconway@gmail.com>
2017-12-21 21:13:55 -0800

    updates

--- lib/resque_web_potion_web/controllers/failed_controller.ex
+++ lib/resque_web_potion_web/controllers/failed_controller.ex
@@ -2,7 +2,9 @@ defmodule ResqueWebPotionWeb.FailedController do
   use ResqueWebPotionWeb, :controller
 
   def index(conn, _params) do
-    queues = ResqueWebPotion.Resque.Queues.queues
-    render conn, "index.html", queues: queues
+    page = 1
+    size = ResqueWebPotion.Resque.Failed.failed_size
+    failures = ResqueWebPotion.Resque.Failed.failed page, 20
+    render conn, "index.html", page: page, size: size, failures: failures
   end
 end
